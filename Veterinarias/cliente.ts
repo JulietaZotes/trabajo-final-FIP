d586efab451e475f390adc180811326fcb1e659b
JuanHMelendez <juanh.melendez@gmail.com>
2023-10-12 11:57:39 -0300

    cliente, paciente

--- Veterinarias/cliente.ts
+++ Veterinarias/cliente.ts
@@ -1,56 +1,44 @@
-import { EntidadVeterinaria } from "./veterinaria";
 import { randomUUID as uid } from "node:crypto";
 
 
-export class Cliente extends EntidadVeterinaria {
-  private clientes: {
-    id: string;
-    nombre: string;
-    telefono: number;
-    esVIP: boolean;
-    visitas: number;
-  }[] = [];
+export class Cliente {
+  private  IdCliente: string = uid();
+  private  NombreCliente: string;
+  private  TelCliente: number;
+  private  EsVIP: boolean;
+  private  Visitas: number;
+  clientes = [];
 
-  public altaCliente(nombre: string, telefono: number): void {
-    const nuevoCliente = {
-      id: uid(),
-      nombre,
-      telefono,
-      esVIP: false,
-      visitas: 0,
-    };
-    this.clientes.push(nuevoCliente);
-  }
+public constructor(NombreCliente : string , TelCliente : number ){
+  this.NombreCliente = NombreCliente ;
+  this.TelCliente = TelCliente ;
 
-  public bajaClientePorId(id: string): void {
-    this.clientes = this.clientes.filter((cliente) => cliente.id !== id);
-  }
-
-  public modificarClientePorId(id: string, nuevoNombre: string, nuevoTelefono: number): void {
-    this.clientes = this.clientes.map((cliente) => {
-      if (cliente.id === id) {
-        return {
-          ...cliente,
-          nombre: nuevoNombre,
-          telefono: nuevoTelefono,
-        };
-      }
-      return cliente;
-    });
-  }
-
-  public marcarComoVIP(id: string): void {
-    this.clientes = this.clientes.map((cliente) => {
-      if (cliente.id === id) {
-        if (cliente.visitas >= 5) {
-          cliente.esVIP = true;
-        }
-        cliente.visitas++;
-      }
-      return cliente;
-    });
-  }
 }
+public Getid(){
+  return this.IdCliente;
+
+}
+
+public SetNombreCliente (NombreCliente : string):void {
+  this.NombreCliente = NombreCliente ;
+}
+
+public GetNombreCliente (){
+  return this.NombreCliente;
+}
+
+public SetTelefonoCliente (TelCliente : number){
+  this.TelCliente = TelCliente ;
+}
+
+public GetTelefonoCliente () {
+  return this.TelCliente;
+}
+
+
+
+}  
+
 ///comentario
 
 
--- Veterinarias/proveedor.ts
+++ Veterinarias/proveedor.ts
@@ -1,6 +1,28 @@
-import { EntidadVeterinaria } from "./veterinaria";
-class Proveedor extends EntidadVeterinaria {
-    constructor(public telefono: string, nombre: string, direccion: string) {
-      super(nombre, direccion);
-    }
+import { randomUUID as uid } from "node:crypto";
+
+class Proveedor  {
+  private NombreProv : string ;
+  private TelefonoProv : number ;
+  private IdProv : string = uid();
+
+  constructor(NombreProv: string, TelefonoProv: number) {
+    this.NombreProv = NombreProv ;
+    this.TelefonoProv = TelefonoProv;
+  }
+  setNombreProv (NombreProv : string) :void{
+    this.NombreProv = NombreProv ;
+  }
+
+  getNombreProv (){
+    return this.NombreProv;
+  }
+
+  setTelefonoProv (TelefonoProv : number):void{
+    this.TelefonoProv = TelefonoProv ;
+  }
+
+  getTelefonoProv () {
+    return this.TelefonoProv ;
+  }
+
   }
