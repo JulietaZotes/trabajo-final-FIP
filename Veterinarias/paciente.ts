d73aaadb0d7e6d485ec79c00d513f7a350f10522
JuanHMelendez <juanh.melendez@gmail.com>
2023-10-13 11:39:21 -0300

    ultimo commit

--- Veterinarias/paciente.ts
+++ Veterinarias/paciente.ts
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-import { randomUUID as uid } from "node:crypto";
-import { Especies } from "./especies";
-
-
-export class Paciente {
-  private id: string;
-  private nombre: string;
-  private especie: Especies[];
-  private idPropietario: string;
-  private pacientes = [];
-
-  public constructor (nombre: string, especie: Especies[]){
-    this.nombre = nombre;
-    this.especie = especie;
-  }
-
-  //DA DE ALTA UN PACIENTE CON EL ID DEL DUENO
-  public altaPaciente(nombre: string, especie: string, idPropietario: number): void {
-    //VALIDACIONES PARA VER SI NO ES PERRO  Y TAMPOCO ES GATO, ES EXOTICA
-    if (especie !== 'perro' && especie !== 'gato') {
-      especie = 'exótica';
-    }
-
-    const nuevoPaciente = {
-      id: uid(),
-      nombre,
-      especie,
-      idPropietario,
-    };
-
-    this.pacientes.push(nuevoPaciente);
-  }
-
-  // BAJA DE PACIENTE POR ID
-  public bajaPacientePorId(id: string): void {
-    this.pacientes = this.pacientes.filter((paciente) => paciente.id !== id);
-  }
-
-  //MODIFICO PACIENTE POR ID
-  public modificarPacientePorId(id: string, nuevaEspecie: string, nuevoIdPropietario: number): void {
-    this.pacientes = this.pacientes.map((paciente) => {
-      if (paciente.id === id) {
-        if (nuevaEspecie !== 'perro' && nuevaEspecie !== 'gato') {
-          nuevaEspecie = 'exótica';
-        }
-
-        return {
-          ...paciente,
-          especie: nuevaEspecie,
-          idPropietario: nuevoIdPropietario,
-        };
-      }
-      return paciente;
-    });
-  }
-}
-=======
 import { randomUUID as uid } from "node:crypto";
 
 export class Paciente {
@@ -106,4 +48,3 @@ export class Paciente {
     });
   }
 }
->>>>>>> 248b5b8a8dea4504b2481b6ad6b57bd9da9286ba
