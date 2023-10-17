3671b5105209f0833d00a3a42609e7bcd5f658b3
enzo952008 <enzo952008@hotmail.com>
2023-10-17 16:35:33 -0300

    cambios generales

--- Veterinarias/cliente.ts
+++ Veterinarias/cliente.ts
@@ -1,61 +1,4 @@
-<<<<<<< HEAD
-import { EntidadVeterinaria } from "./veterinaria";
-import { randomUUID as uid } from "node:crypto";
 
-
-export class Cliente extends EntidadVeterinaria {
-  private clientes: {
-    id: string;
-    nombre: string;
-    telefono: number;
-    esVIP: boolean;
-    visitas: number;
-  }[] = [];
-
-  public altaCliente(nombre: string, telefono: number): void {
-    const nuevoCliente = {
-      id: uid(),
-      nombre,
-      telefono,
-      esVIP: false,
-      visitas: 0,
-    };
-    this.clientes.push(nuevoCliente);
-  }
-
-  public bajaClientePorId(id: string): void {
-    this.clientes = this.clientes.filter((cliente) => cliente.id !== id);
-  }
-
-  public modificarClientePorId(id: string, nuevoNombre: string, nuevoTelefono: number): void {
-    this.clientes = this.clientes.map((cliente) => {
-      if (cliente.id === id) {
-        return {
-          ...cliente,
-          nombre: nuevoNombre,
-          telefono: nuevoTelefono,
-        };
-      }
-      return cliente;
-    });
-  }
-
-  public marcarComoVIP(id: string): void {
-    this.clientes = this.clientes.map((cliente) => {
-      if (cliente.id === id) {
-        if (cliente.visitas >= 5) {
-          cliente.esVIP = true;
-        }
-        cliente.visitas++;
-      }
-      return cliente;
-    });
-  }
-}
-///comentario
-
-
-=======
 import { randomUUID as uid } from "node:crypto";
 
 
@@ -92,13 +35,9 @@ public SetTelefonoCliente (TelCliente : number){
 public GetTelefonoCliente () {
   return this.TelCliente;
 }
-
-
-
 }  
 
 ///comentario
 
 
 //funcionaaaaasdasd
->>>>>>> 248b5b8a8dea4504b2481b6ad6b57bd9da9286ba
