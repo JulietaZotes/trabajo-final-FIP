dfddc5ed5e87ff9de09cd38984c9087cf40d165f
julietazotes <julietaazotes@gmail.com>
2023-10-25 20:32:21 -0300

    add funcion <isVIP>

--- Veterinarias/cliente.ts
+++ Veterinarias/cliente.ts
@@ -1,60 +1,4 @@
-<<<<<<< HEAD
-import { EntidadVeterinaria } from "./veterinaria";
-import { randomUUID as uid } from "node:crypto";
-
-
-export class Cliente extends EntidadVeterinaria {
-  private clientes: {
-    id: string;
-    nombre: string;
-    telefono: number;
-    esVIP: boolean;
-    visitas: number;
-  }[] = [];
-
-  public altaCliente(nombre: string, telefono: number): void {
-    const nuevoCliente = {
-      id: uid(),
-      nombre,
-      telefono,
-      esVIP: false,
-      visitas: 0,
-    };
-    this.clientes.push(nuevoCliente);
-  }
-
-  public bajaClientePorId(id: string): void {
-    this.clientes = this.clientes.filter((cliente) => cliente.id !== id);
-  }
-
-  public modificarClientePorId(id: string, nuevoNombre: string, nuevoTelefono: number): void {
-    this.clientes = this.clientes.map((cliente) => {
-      if (cliente.id === id) {
-        return {
-          ...cliente,
-          nombre: nuevoNombre,
-          telefono: nuevoTelefono,
-        };
-      }
-      return cliente;
-    });
-  }
-
-  public marcarComoVIP(id: string): void {
-    this.clientes = this.clientes.map((cliente) => {
-      if (cliente.id === id) {
-        if (cliente.visitas >= 5) {
-          cliente.esVIP = true;
-        }
-        cliente.visitas++;
-      }
-      return cliente;
-    });
-  }
-}
-
-
-=======
+import { log } from "node:console";
 import { randomUUID as uid } from "node:crypto";
 
 
@@ -69,6 +13,8 @@ export class Cliente {
 public constructor(NombreCliente : string , TelCliente : number ){
   this.NombreCliente = NombreCliente ;
   this.TelCliente = TelCliente ;
+  this.EsVIP = false;
+  this.Visitas = 0
 
 }
 public Getid(){
@@ -92,7 +38,13 @@ public GetTelefonoCliente () {
   return this.TelCliente;
 }
 
-
+public isVIP(){
+  if (this.EsVIP) {
+    return ("Si");
+  } else {
+    return ("No");
+  }
+}
 
 }  
 
@@ -100,4 +52,3 @@ public GetTelefonoCliente () {
 
 
 //funcionaaaaasdasd
->>>>>>> 248b5b8a8dea4504b2481b6ad6b57bd9da9286ba
