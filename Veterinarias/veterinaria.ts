d81fb8bd190a9c64fde1de063d33e568f9ee3538
julietazotes <julietaazotes@gmail.com>
2023-10-25 20:33:19 -0300

    add funcion <showClientes> y <addCliente>

--- Veterinarias/veterinaria.ts
+++ Veterinarias/veterinaria.ts
@@ -2,17 +2,23 @@ import { Especies } from "./especies";
 import { Perros } from "./perros";
 import { Gatos } from "./gatos";
 import { Exoticos } from "./exoticos";
+import * as rls from "readline-sync";
+import { Cliente } from "./cliente";
+import { fileManager } from "./fileManager";
+import { log } from "console";
 
 export class Veterinaria {
   private direccion:string;
   private nombre:string;
   private telefono:number;
   private especies: Especies[];
+  private clientes: Cliente[];
   public constructor(nombre:string,direccion:string, telefono:number) {
       this.nombre = nombre;
       this.direccion = direccion;
       this.telefono = telefono;
       this.especies = [];
+      this.clientes = [];
     };
     
   public addPaciente(paciente: Especies){
@@ -50,10 +56,42 @@ export class Veterinaria {
     Telefono: ${this.telefono}
     `);
   }
+
+  public addCliente(newCliente: Cliente){
+
+    this.clientes.push(newCliente);
+    fileManager.readClientes();
+    fileManager.appendClientes(this.clientes);
+  }
+
+  //async es una palabra clave que se utiliza para declarar una función asincrónica. Una función asincrónica es una función que realiza operaciones asincrónicas, como operaciones de lectura/escritura de archivos.
+  public showClientes(){
+    const readResult = fileManager.readClientes(); //
+    if(readResult){
+      console.log("\n------Clientes------\n");
+      if(!this.clientes.length){
+        console.log("No se encontraron clientes.\n");
+      } else {
+        this.clientes.forEach((cliente) => {
+          console.log(`
+          ID: ${cliente.Getid()}
+          Nombre: ${cliente.GetNombreCliente()}
+          Telefono: ${cliente.GetTelefonoCliente()}
+          VIP: ${cliente.isVIP()}
+          ------
+          `);     
+        });
+      } 
+    }
+  }
 }
 
 const vete01 = new Veterinaria("vete 1", "av123", 1223444);
-const perro01 = new Perros("golden", "macho", "3 meses");
-const exotico01 = new Exoticos("piton", "macho", "10 años", "vibora")
-vete01.addPaciente(perro01);
-vete01.addPaciente(exotico01);
+// const perro01 = new Perros("golden", "macho", "3 meses");
+// const exotico01 = new Exoticos("piton", "macho", "10 años", "vibora")
+// vete01.addPaciente(perro01);
+// vete01.addPaciente(exotico01);
+
+const cliente01 = new Cliente("Ana Rodriguez", 123456);
+vete01.addCliente(cliente01);
+vete01.showClientes();
